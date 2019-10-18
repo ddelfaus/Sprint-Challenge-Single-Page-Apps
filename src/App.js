3657ee382b93c477c499595d97c55cef3c2ded6d
ddelfaus <ddelfaus@gmail.com>
2019-10-18 11:42:39 -0400

    step 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList"
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
-    </main>
+      <Route exact path = "/" component ={WelcomePage}/>
+      <Route path = "/CharacterList/" component = {CharacterList}/>
+       </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import  { Route } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
+
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      
+      <div className = "head-links">
+      <Link to="/">Home</Link>
+      <Link to="/CharacterList/">Characters</Link>
+
+
+      </div>
+
     </header>
   );
 }
